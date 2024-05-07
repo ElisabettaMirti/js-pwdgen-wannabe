0d55fd8334758ec34b7d8fc9f02f1ec28d07f30c
ElisabettaMirti <belmirti@gmail.com>
2024-05-07 15:59:03 +0200

    first commit

---
+++ js/fileJS.js
@@ -0,0 +1,15 @@
+// Chiedi all’utente/essa il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito Infine scrivi sulla pagina:
+//  nomecognomecolorepreferito24
+
+
+const userName = prompt("Inserisci il tuo nome");
+
+const userSurname = prompt("Inserisci il tuo cognome");
+
+const userColor = prompt("Qual è il tuo colore preferito?");
+
+console.log(
+    `
+    ${userName}${userSurname}${userColor}24
+    `
+);
